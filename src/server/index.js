576cbd9cf648c5668acc897079a975c8511667ec
TheTeejers <30870766+TheTeejers@users.noreply.github.com>
2018-10-23 15:11:55 -0500

    fixed server issue

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-const express = require('expres')
+const express = require('express')
 const app = express()
 const server = require('http').createServer()
 const io = module.exports.io = require('socket.io')(server)
