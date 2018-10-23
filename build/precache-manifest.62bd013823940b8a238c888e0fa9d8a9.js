2fcca6f35b98237297028dbf8561ac91f68eeb3c
TheTeejers <30870766+TheTeejers@users.noreply.github.com>
2018-10-23 16:15:13 -0500

    trying again

---
+++ build/precache-manifest.62bd013823940b8a238c888e0fa9d8a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "177bdf774ba7d88b6d9d",
+    "url": "/static/js/main.177bdf77.chunk.js"
+  },
+  {
+    "revision": "620ecc40827dcf4b9d2b",
+    "url": "/static/js/1.620ecc40.chunk.js"
+  },
+  {
+    "revision": "177bdf774ba7d88b6d9d",
+    "url": "/static/css/main.ab5b3a71.chunk.css"
+  },
+  {
+    "revision": "51c0d9c461af9e3ae96410e3c3f6eae1",
+    "url": "/index.html"
+  }
+];
--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const io = module.exports.io = require('socket.io')(server)
 const PORT = process.env.PORT || 3231
 
 const SocketManager = require('./SocketManager')
-app.use(express.static(__dirname + '/../../build/index.html'))
+app.use(express.static(__dirname + '/../../build'))
 io.on('connection', SocketManager)
 
 server.listen(PORT, ()=>{
